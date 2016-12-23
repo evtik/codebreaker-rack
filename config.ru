bdfb65e986630caa8dbe0927d3c2ed90023e8c8a
Serhii <e.v.t.i.k@ukr.net>
2016-12-23 11:13:01 +0200

    Setup Slim template engine

--- config.ru
+++ config.ru
@@ -1,2 +1,7 @@
 require './lib/racker'
+use Rack::Static, urls: ['/css'], root: 'public'
+use Rack::Session::Cookie, key: 'rack.session',
+                           path: '/',
+                           secret: '#lkjdf5234%'
+Slim::Engine.set_options pretty: true
 run Racker.new
--- lib/racker.rb
+++ lib/racker.rb
@@ -1,12 +1,31 @@
 require 'slim'
+require 'codebreaker'
 
 class Racker
   def call(env)
-    Rack::Response.new(render('index.slim'))
+    @request = Rack::Request.new(env)
+    case @request.path
+    when '/' then index_page
+    when '/game' then new_game
+    else Rack::Response.new('Not found', 404)
+    end
   end
 
-  def render(template)
+  def index_page
+    xyz = 'zyx'
+    Rack::Response.new(render('index.slim', aaa: xyz))
+  end
+
+  def new_game
+    @game = Codebreaker::Game.new
+    @digits = '12345'
+    Rack::Response.new(render('game.slim', game: @game, digits: @digits))
+  end
+
+  def render(template, data = {})
+    @layout ||= Slim::Template
+                .new(File.expand_path('../views/layout.slim', __FILE__))
     path = File.expand_path("../views/#{template}", __FILE__)
-    Slim::Template.new(path).render
+    @layout.render { Slim::Template.new(path).render(Object.new, data) }
   end
 end
